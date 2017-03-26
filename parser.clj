9baa2bb689f47bfdf370c54c510eabfad4051d9d
Sobia Adil <sobia.hassen@gmail.com>
2017-03-26 22:20:55 +0530

    Add value-parser

--- parser.clj
+++ parser.clj
@@ -1,3 +1,19 @@
+;; constants
+
+;; regexes
+(def number-regex          #"^(\d+)((.|\n)*)$")
+(def string-regex          #"^(\"[^\\']*(?:\\.[^\\']*)*\")((.|\n)*)$")
+(def bool-regex            #"^(true|false)((.|\n)*)$")
+(def null-regex            #"^(null)((.|\n)*)$")
+(def space-regex           #"^([ \t]+)((.|\n)*)$")
+(def open-sqr-regex        #"^(\[)((.|\n)*)$")
+(def close-sqr-regex       #"^(\])((.|\n)*)$")
+(def open-curly-regex      #"^(\{)((.|\n)*)$")
+(def close-curly-regex     #"^(\{)((.|\n)*)$")
+
+;; parser vector for the value parser
+(def parsers [number-parser string-parser bool-parser]) ;; add array parser and value parser
+
 ;; utility functions
 
 (defn get-match [regex]
@@ -10,7 +26,11 @@
 		nil
 		(func value)))
 
+(defn get-first-match [input]
+	(filter #(not (= nil)) (map #(% input) parsers)))
+
 ;; value extractors 
+
 (defn get-number [value]
     (update value 0 #(Integer/parseInt %)))
 
@@ -26,35 +46,35 @@
 ;; matcher functions
 
 (def match-number
-	(get-match #"^(\d+)((.|\n)*)$"))
+	(get-match number-regex))
 
 (def match-string
-	(get-match #"^(\"[^\\']*(?:\\.[^\\']*)*\")((.|\n)*)$"))
+	(get-match string-regex))
 
 (def match-bool
-	(get-match #"^(true|false)((.|\n)*)$"))
+	(get-match bool-regex))
 
 (def match-null
-	(get-match #"^(null)((.|\n)*)$"))
-
+	(get-match null-regex))
 
 ;; basic parsers
 
 (def space-parser
-	(get-match #"^([ \t]+)((.|\n)*)$"))
+	(get-match space-regex))
 
 (def open-sqr-bkt
-	(get-match #"^(\[)((.|\n)*)$"))
+	(get-match open-sqr-regex))
 
 (def close-sqr-bkt
-	(get-match #"^(\])((.|\n)*)$"))
+	(get-match close-sqr-regex
 
 (def open-curly-bkt
-	(get-match #"^(\{)((.|\n)*)$"))
+	(get-match open-curly-regex
 
 (def close-curly-bkt
-	(get-match #"^(\{)((.|\n)*)$"))
+	(get-match close-curly-regex)
 
+(def comma-parser)
 ;; value parsers
 
 (defn number-parser [input]
@@ -68,3 +88,9 @@
 
 (defn null-parser [input]
 	(maybe (match-null input) get-null))
+
+;; parser-array for the value-parser
+
+(defn value-parser [input]
+	(get-first-match input))
+
